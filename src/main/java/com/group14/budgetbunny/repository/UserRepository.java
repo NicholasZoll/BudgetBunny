0c1179105645e1ba9976a23b23d84fb2fe620a5b
nzoll <nickzll@yahoo.com>
2024-10-31 17:32:00 -0400

    user table added, first edition

---
+++ src/main/java/com/group14/budgetbunny/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.group14.budgetbunny.repository;
+
+import com.group14.budgetbunny.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface UserRepository extends JpaRepository<User, Long> {
+}
