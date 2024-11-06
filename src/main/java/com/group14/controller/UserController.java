5e35ac55fef579ebf4520f32c1634f3e2db6da6f
nzoll <nickzll@yahoo.com>
2024-11-06 15:47:19 -0500

    createaccount functionality, creates users and adds them to the db

--- src/main/java/com/group14/controller/UserController.java
+++ src/main/java/com/group14/controller/UserController.java
@@ -2,32 +2,22 @@ package com.group14.controller;
 
 import com.group14.budgetbunny.model.User;
 import com.group14.budgetbunny.repository.UserRepository;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
-    
+
     @Autowired
     UserRepository repository;
 
     @PostMapping("/addUser")
-    public void addUser(@RequestBody User user) {
-        repository.save(user);
+    public User addUser(@RequestBody User user) {
+        return repository.save(user);
     }
+
     @GetMapping("/test")
     public String testEndpoint() {
         return "Test endpoint working";
     }
-
 }
