7c74d1bdfa1bab0a4dbc1d1ef0b03a0a0d272e36
nzoll <nickzll@yahoo.com>
2024-11-06 16:02:32 -0500

    login functionality, session storage not implemented yet

--- src/main/java/com/group14/controller/UserController.java
+++ src/main/java/com/group14/controller/UserController.java
@@ -3,7 +3,11 @@ package com.group14.controller;
 import com.group14.budgetbunny.model.User;
 import com.group14.budgetbunny.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -14,6 +18,16 @@ public class UserController {
     @PostMapping("/addUser")
     public User addUser(@RequestBody User user) {
         return repository.save(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User loginUser) {
+        Optional<User> user = repository.findByEmail(loginUser.getEmail());
+        if (user.isPresent() && user.get().getPassword().equals(loginUser.getPassword())) {
+            return ResponseEntity.ok("Login successful");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+        }
     }
 
     @GetMapping("/test")
