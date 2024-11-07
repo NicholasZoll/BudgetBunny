ad081a976841202027f89a08cfdb6f1d8a76a574
nzoll <nickzll@yahoo.com>
2024-11-07 02:44:58 -0500

    session storage functionality added (hopefully), after logging in index.html shows username of logged in user

--- src/main/java/com/group14/controller/UserController.java
+++ src/main/java/com/group14/controller/UserController.java
@@ -2,6 +2,9 @@ package com.group14.controller;
 
 import com.group14.budgetbunny.model.User;
 import com.group14.budgetbunny.repository.UserRepository;
+
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,9 +24,10 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody User loginUser) {
+    public ResponseEntity<String> login(@RequestBody User loginUser, HttpSession session) {
         Optional<User> user = repository.findByEmail(loginUser.getEmail());
         if (user.isPresent() && user.get().getPassword().equals(loginUser.getPassword())) {
+            session.setAttribute("username", user.get().getEmail());
             return ResponseEntity.ok("Login successful");
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
@@ -33,5 +37,15 @@ public class UserController {
     @GetMapping("/test")
     public String testEndpoint() {
         return "Test endpoint working";
+    }
+
+    @GetMapping("/getUserEmail")
+    public ResponseEntity<String> getUserEmail(HttpSession session) {
+        String email = (String) session.getAttribute("username");
+        if (email != null) {
+            return ResponseEntity.ok(email);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not logged in");
+        }
     }
 }
