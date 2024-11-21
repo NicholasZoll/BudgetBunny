a21c472b67de9ab1f171f5e3089c1dd22858ab12
nzoll <nickzll@yahoo.com>
2024-11-21 10:51:42 -0500

    removed env call from application file

--- src/main/java/com/group14/budgetbunny/BudgetbunnyApplication.java
+++ src/main/java/com/group14/budgetbunny/BudgetbunnyApplication.java
@@ -11,17 +11,17 @@ public class BudgetbunnyApplication {
 
 	public static void main(String[] args) {
 		// Load environment variables from .env
-		Dotenv dotenv = Dotenv.configure().load();
+		// Dotenv dotenv = Dotenv.configure().load();
 
-		// test print
-		System.out.println("Database URL: " + dotenv.get("DATABASE_URL"));
-    	System.out.println("Database Username: " + dotenv.get("DATABASE_USERNAME"));
-		System.out.println("Database Username: " + dotenv.get("DATABASE_PASSWORD"));
+		// // test print
+		// System.out.println("Database URL: " + dotenv.get("DATABASE_URL"));
+    	// System.out.println("Database Username: " + dotenv.get("DATABASE_USERNAME"));
+		// System.out.println("Database Username: " + dotenv.get("DATABASE_PASSWORD"));
 
-		// Set environment variables explicitly if needed (optional step)
-		System.setProperty("spring.datasource.url", dotenv.get("DATABASE_URL"));
-		System.setProperty("spring.datasource.username", dotenv.get("DATABASE_USERNAME"));
-		System.setProperty("spring.datasource.password", dotenv.get("DATABASE_PASSWORD"));
+		// // Set environment variables explicitly if needed (optional step)
+		// System.setProperty("spring.datasource.url", dotenv.get("DATABASE_URL"));
+		// System.setProperty("spring.datasource.username", dotenv.get("DATABASE_USERNAME"));
+		// System.setProperty("spring.datasource.password", dotenv.get("DATABASE_PASSWORD"));
 
 		SpringApplication.run(BudgetbunnyApplication.class, args);
 	}
